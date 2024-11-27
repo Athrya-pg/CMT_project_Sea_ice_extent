bf5b18de6f7d4dd239251a292bdf0581b1cd75f1
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-11-27 15:55:39 +0100

    Changement de nom

--- calculate_R2.c
+++ calculate_R2.c
@@ -1,8 +1,20 @@
+
+#include <stdio.h>
+#include <math.h>
+// Function to calculate the mean
+double mean(double *values, int n) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        sum += values[i];
+    }
+    return sum / n;
+}
+
 //Function to calculate R2, the coefficient of determination
 double calculate_r2(double* y_true, double* y_pred, int n){
     double ss_res = 0.0;
     double ss_tot = 0.0;
-    double y_mean = mean(y_true, n);
+    int y_mean = mean(y_true, n);
 
     for (int i = 0; i < n; i++) {
         ss_res += pow(y_true[i] - y_pred[i], 2);
--- main.c
+++ main.c
@@ -2,83 +2,107 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-#include "Linear_Regression.h"
+#include "Linear_Regression.c"
 #include "calculate_RMSE.c"
 #include "calculate_R2.c"
 
 // Function pour read the data from the file
-int read_data(const char *filename, double *values, int max_size) {
+int read_data(const char *filename, double *values, double *time, int max_size) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
+        perror("Error opening the file");
         return -1;
     }
-    int count = 0;
-    while (fscanf(file, "%lf", &values[count]) == 1 && count < max_size) {
+     int count = 0;
+    while (count < max_size) {
+        if (fscanf(file, "%lf,%lf", &values[count], &time[count]) != 2) {
+            break;
+        }
         count++;
     }
     fclose(file);
     return count;
 }
-// Function to calculate the mean
-double mean(double *values, int n) {
-    double sum = 0.0;
-    for (int i = 0; i < n; i++) {
-        sum += values[i];
-    }
-    return sum / n;
-}
 
 int main(){
     // x are green gas emmisions
-    // y are sea ice extent
-    const int max_data_size = 100;
+    // yN and yS are sea ice extent
+    const int max_data_size = 1000;
+    double t[max_data_size];
     double x[max_data_size]; 
-    double y[max_data_size];
-    int n_x, n_y;
+    double yN[max_data_size];
+    double yS[max_data_size];
+    int n_x, n_yN;
 
     // Read data from sea ice
-    n_y = read_data("sea_ice_sh.csv", y, max_data_size);
-    if (n_y <= 0) {
-        printf("Error: no data in sea_ice.csv\n");
+    int n_yS = read_data("sea_ice_sh.csv", yS, t, max_data_size);
+    printf("n_yS = %d\n", n_yS);
+    if (n_yS <= 0) {
+        printf("Error: no data in sea_ice_sh.csv\n");
+        return 1;
+    }
+
+    n_yN = read_data("sea_ice_nh.csv", yN, t, max_data_size);
+    if (n_yN <= 0) {
+        printf("Error: no data in sea_ice_nh.csv\n");
         return 1;
     }
 
     // Read data from GHG
-    n_x = read_data("summed_co2.csv", x, max_data_size);
+    n_x = read_data("summed_co2.csv", x, t, max_data_size);
     if (n_x <= 0) {
         printf("Error : no data in summed_co2.csv\n");
         return 1;
     }
 
-    // Check if the number of data is the same
-    if (n_x != n_y) {
-        printf("Error : files don't have the same number of data points (%d vs %d).\n", n_x, n_y);
+    // Check if the number of data points is the same
+    if (n_x != n_yN || n_x != n_yS) {
+        printf("Error: files don't have the same number of data points (%d vs %d vs %d).\n", n_x, n_yN, n_yS);
         return 1;
     }
+
     // Number of data points
     int n = n_x;
 
-    // calulate the linear regression
-    double m, b;
-    Linear_regression(x, y, n_x, &m, &b);
+    // Calculate the linear regression for northern hemisphere
+    double mN, bN;
+    Linear_Regression(x, yN, n, &mN, &bN);
 
-    // Print the result
-    printf("Équation de la régression linéaire : y = %.2fx + %.2f\n", m, b);
+    // Calculate the linear regression for southern hemisphere
+    double mS, bS;
+    Linear_Regression(x, yS, n, &mS, &bS);
 
-    // Calculate the predictions
-    double y_pred[max_data_size];
-    for (int i = 0; i < n_x; i++) {
-        y_pred[i] = m * x[i] + b;
+    // Output the results
+    printf("Northern Hemisphere: y = %lf * x + %lf\n", mN, bN);
+    printf("Southern Hemisphere: y = %lf * x + %lf\n", mS, bS);
+
+    // Calculate the predictions and errors for northern hemisphere
+    double yN_pred[max_data_size];
+    for (int i = 0; i < n; i++) {
+        yN_pred[i] = mN * x[i] + bN;
     }
-    
-    // calculate the mean squared error
-    double MSE = calculate_mse(y, y_pred, n);
-    printf("Mean Squared Error (MSE): %f\n", MSE);
+    double RMSE_N = calculate_rmse(yN, yN_pred, n);
+    double R2_N = calculate_r2(yN, yN_pred, n);
+    printf("Northern Hemisphere RMSE: %f\n", RMSE_N);
+    printf("Northern Hemisphere R2: %f\n", R2_N);
 
-    // calculate the coefficient of determination
-    double R2 = calculate_r2(y, y_pred, n);
-    printf("Coefficient of determination (R2): %f\n", R2);
+    // Calculate the predictions and errors for southern hemisphere
+    double yS_pred[max_data_size];
+    for (int i = 0; i < n; i++) {
+        yS_pred[i] = mS * x[i] + bS;
+    }
+    double RMSE_S = calculate_rmse(yS, yS_pred, n);
+    double R2_S = calculate_r2(yS, yS_pred, n);
+    printf("Southern Hemisphere RMSE: %f\n", RMSE_S);
+    printf("Southern Hemisphere R2: %f\n", R2_S);
+    
+    // // // calculate the mean squared error
+    // double MSE = calculate_mse(y, y_pred, n);
+    // printf("Mean Squared Error (MSE): %f\n", MSE);
+
+    // // calculate the coefficient of determination
+    // double R2 = calculate_r2(y, y_pred, n);
+    // printf("Coefficient of determination (R2): %f\n", R2);
 
     return 0;
 }
