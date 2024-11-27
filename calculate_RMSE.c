c877bcc8a1de4d6e7dc1149596995d2b05da6c2d
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-11-27 14:53:43 +0100

    Create calculate_RMSE.c

---
+++ calculate_RMSE.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to calculate the root mean square error, RMSE
+double calculate_rmse(double *y_obs, double *y_pred, int n){
+    double RMSE = 0.0;
+    for(int i = 0; i<n; i++){
+        RMSE += pow(y_obs[i] - y_pred[i], 2);
+    }
+    return sqrt(RMSE / n);
+}
