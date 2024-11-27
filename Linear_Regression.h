2c0b5efe7640f4442f1e18aaaf08c045f526f8f8
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-11-27 14:53:54 +0100

    Create Linear_Regression.h

---
+++ Linear_Regression.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to calulate the linear regression
+// y = b0x0 + b1x1 + c
+// y is the sea ice extent
+// x is the green gas emmisions
+
+void Linear_Regression(double *x, double *y, int n, double *m, double *b){
+    double sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0;
+    for(int i = 0; i < n; i++){
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += x[i] * y[i];
+        sum_x2 += x[i] * x[i];
+    }
+    *m = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x * sum_x);
+    *b = (sum_y - *m * sum_x) / n;
+}
