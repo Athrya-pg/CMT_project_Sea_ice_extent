7109a6c91d98bef23d567741c0f30f91d426d480
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-12-06 13:14:14 +0100

    Update Visualisation.py

--- Visualisation.py
+++ Visualisation.py
@@ -6,7 +6,7 @@ import pandas as pd
 observations_nh = pd.read_csv('outputs/sea_ice_nh.csv')  # Observation of Northern Hemisphere
 observations_sh = pd.read_csv('outputs/sea_ice_sh.csv')  # Observation of Southern Hemisphere
 co2_data = pd.read_csv('outputs/summed_co2.csv')  # Emissions de CO2
-estimations = pd.read_csv('yestimation.csv')  # Estimations of Northern Hemisphere
+estimations = pd.read_csv('outputs/yestimation.csv')  # Estimations of Northern Hemisphere
 
 # CO2 = co2_data['CO2_Mass']
 # t = co2_data['Year']
@@ -37,12 +37,12 @@ else:
     print("Les colonnes d'estimations sont manquantes dans le fichier CSV.")
     exit(1)
 # Extraire les colonnes nécessaires pour les observations
-if 'Annual' in observations_nh.columns and 'Annual' in observations_sh.columns:
-    y_obs_nh = observations_nh['Annual']  # Observations de l'Hémisphère Nord
-    y_obs_sh = observations_sh['Annual']  # Observations de l'Hémisphère Sud
-    print("Colonnes 'Annual' trouvées et extraites pour les observations.")
+if 'Year' in observations_nh.columns and 'Year' in observations_sh.columns:
+    y_obs_nh = observations_nh['Year']  # Observations de l'Hémisphère Nord
+    y_obs_sh = observations_sh['Year']  # Observations de l'Hémisphère Sud
+    print("Colonnes 'Year' trouvées et extraites pour les observations.")
 else:
-    print("Les colonnes 'Annual' sont manquantes dans les fichiers d'observations.")
+    print("Les colonnes 'Year' sont manquantes dans les fichiers d'observations.")
     exit(1)
 
 #Plot yN vs CO2 with y_estim_nh
@@ -54,7 +54,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title(' Global CO2 emissions vs Arctic Sea Ice Extent (Northern Hemisphere)')
 plt.legend(loc ='upper right')
 plt.grid(True)
-plt.savefig('LinearReg_CO2_vs_ICE_NH_year.png')
+plt.savefig('outputs/LinearReg_CO2_vs_ICE_NH_year.png')
 
 #Plot yS vs CO2 with y_estim_sh
 plt.figure(figsize=(10, 6))
@@ -65,7 +65,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title(' Global CO2 emissions vs Arctic Sea Ice Extent (Southern Hemisphere)')
 plt.legend(loc ='upper right')
 plt.grid(True)
-plt.savefig('LinearReg_CO2_vs_ICE_SH_year.png')
+plt.savefig('outputs/LinearReg_CO2_vs_ICE_SH_year.png')
 
 #Plot yN vs t with y_estim_nh
 plt.figure(figsize=(10, 6))
@@ -76,7 +76,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title('Year vs Sea Ice Extent (Northern Hemisphere)')
 plt.legend(loc='upper right')
 plt.grid(True)
-plt.savefig('Year_vs_IceExtent_NH.png')
+plt.savefig('outputs/Year_vs_IceExtent_NH.png')
 
 #Plot yS vs t with y_estim_sh
 plt.figure(figsize=(10, 6))
@@ -87,7 +87,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title('Year vs Sea Ice Extent (Southern Hemisphere)')
 plt.legend(loc='upper right')
 plt.grid(True)
-plt.savefig('Year_vs_IceExtent_SH.png')
+plt.savefig('outputs/Year_vs_IceExtent_SH.png')
 
 #Plot ySpoly vs CO2 with y_estim_sh
 plt.figure(figsize=(10, 6))
@@ -98,7 +98,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title('CO2 Emissions vs Sea Ice Extent (Southern Hemisphere) - Polynomial Regression')
 plt.legend(loc='upper right')
 plt.grid(True)
-plt.savefig('CO2_vs_IceExtent_SH_Poly.png')
+plt.savefig('outputs/CO2_vs_IceExtent_SH_Poly.png')
 
 # Plot ySpoly vs t
 plt.figure(figsize=(10, 6))
@@ -109,5 +109,7 @@ plt.ylabel('Ice Surface (million km²)', fontsize=12)
 plt.title('Year vs Sea Ice Extent (Southern Hemisphere) - Polynomial Regression')
 plt.legend(loc='upper right')
 plt.grid(True)
-plt.savefig('Year_vs_IceExtent_SH_Poly.png')
+plt.savefig('outputs/Year_vs_IceExtent_SH_Poly.png')
+
+print("Plot saved in outputs folder")
 
