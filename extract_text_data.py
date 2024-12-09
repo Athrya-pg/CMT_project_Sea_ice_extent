f72b58de5c0f52ab80742c10b87ca2421e04a3e9
baechler-ali <alicia.bachler@epfl.ch>
2024-12-09 11:25:46 +0100

    Extract Data Ocean Temperature

--- extract_text_data.py
+++ extract_text_data.py
@@ -5,9 +5,44 @@ data_folder = 'data/'
 output_folder = 'outputs/'
 
 # Create input and output file
-input = 'nino'
-output = 'nino.csv'
+input = 'aravg.ann.ocean.90S.60S.v6.0.0.202410.asc'
+output = 'ocean_temp_annual_SPole.csv'
 
+list_entries = []
+# Reading an ASCII file
+with open(data_folder + input, 'r') as file:
+    content = file.readlines()
+    #print(content)
+    for line in content:
+        #print(line.strip())
+        list_entries.append(line.strip())
+#print(list_entries)
+
+split_data = []
+
+for row in list_entries:
+    values = row.strip().split()
+    years = values[0]
+    for i,value in enumerate(values[1:]):
+        split_data.append([f'{years}',value])
+
+temp_df = pd.DataFrame(split_data, columns=['Year','Value_T'])
+#if temp_df['Value'] == '-999.000000':
+temp_df = temp_df[temp_df['Value_T'] != '-999.000000']
+temp_df.reset_index(drop=True, inplace=True)
+
+#print(temp_df[120:130])
+
+ocean_temp_cut = temp_df[129:174]
+ocean_temp_cut.reset_index(drop=True, inplace=True)
+print(ocean_temp_cut)
+
+ocean_temp_cut.to_csv(output_folder + output, index=False)
+print('Accomplished.')
+
+
+
+'''
 # Read the text file
 with open(data_folder + input, 'r') as file:
     rows = file.readlines()
@@ -37,6 +72,6 @@ nino_df = pd.DataFrame(timeXdata, columns=['Date','Std_Value'])
 
 # Convert to CSV file
 nino_df.to_csv(output_folder+output, index=False)
-
+'''
 
 
