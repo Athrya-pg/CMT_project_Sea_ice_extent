ce2deb43e05bf99bd9093ab89599b9cd8d204d60
baechler-ali <alicia.bachler@epfl.ch>
2024-12-11 16:02:59 +0100

    Version Control 4

---
+++ Extract_Data.py
@@ -0,0 +1,221 @@
+### Extracting Data ###
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from sklearn.preprocessing import StandardScaler
+
+# Set I/O folder names
+data_folder = 'data/'
+output_folder = 'outputs/'
+
+# Set dataset file names for the input datasets
+# -------------------------------------------------------
+
+
+# --------- Extracting Data from an Excelsheet (.xlsx) ---------------------------------------------------------------------
+
+# Read the excel file into a dataFrame
+df_nh = pd.read_excel(data_folder + 'Sea_Ice_Index_Monthly_Data_by_Year_G02135_v3.0.xlsx', sheet_name='NH-Extent')
+df_sh = pd.read_excel(data_folder + 'Sea_Ice_Index_Monthly_Data_by_Year_G02135_v3.0.xlsx', sheet_name='SH-Extent') 
+df_co2 = pd.read_excel(data_folder + 'IEA_EDGAR_CO2_1970_2023.xlsx', sheet_name='IPCC 2006')
+print('Excel read.')
+
+
+# Define the columns and rows we want from the data sheet
+df_nhcut= df_nh.iloc[1:46,14:15].reset_index(drop=True)
+df_shcut= df_sh.iloc[1:46,14:15].reset_index(drop=True)
+df_co2cut= df_co2.iloc[9:3539, 17:62].reset_index(drop=True)
+print('Dataframe cut.')
+
+
+# Rename dataframe columns to fit our needs
+df_nhcut.rename(columns={'Annual': 'NH_Extent'}, inplace=True)
+df_shcut.rename(columns={'Annual': 'SH_Extent'}, inplace=True)
+print('Column renamed.')
+
+
+# Summing individual country CO2 emmissions to get the total CO2 emmissions for each year
+# And makine sure it is a dataframe 
+sum_co2 = df_co2cut.sum()
+sum_co2_df = pd.DataFrame(sum_co2, columns=['CO2'])
+
+# Resting the row index
+sum_co2_df.reset_index(drop=True, inplace=True)
+
+# Adjusting the data from Gg to Gt for future calculation purposes
+sum_co2_df['CO2'] = sum_co2_df['CO2'] * 0.000001
+print('CO2 total calculated.')
+
+
+# Adding a 'Year' column to be able to keep track of the data acurately
+years = list(range(1979, 2024))
+sum_co2_df['Year'] = years
+df_nhcut['Year'] = years
+df_shcut['Year'] = years
+print('Years column added.')
+
+
+# Ensure the data is numeric and handle missing values
+df_nhcut['NH_Extent'] = pd.to_numeric(df_nhcut['NH_Extent'], errors='coerce')
+df_shcut['SH_Extent'] = pd.to_numeric(df_shcut['SH_Extent'], errors='coerce')
+sum_co2_df['CO2'] = pd.to_numeric(sum_co2_df['CO2'], errors='coerce')
+print('To numeric.')
+
+ 
+# Converting the dataframes to CSV files for safe keeping
+df_nhcut.to_csv(output_folder + 'sea_ice_nh.csv', index=False)
+df_shcut.to_csv(output_folder + 'sea_ice_sh.csv', index=False)
+sum_co2_df.to_csv(output_folder + 'summed_co2.csv', index=False)
+print('Converted to csv.')
+
+
+
+# --------- Ploting Sea Ice Extent and CO2 Emissions Over Time --------------------------------------------------------------
+# We are ploting those graphs to see if it is possible to use a linear regression for our approximation.
+
+# Read the CSV files into DataFrames
+df_nhcut = pd.read_csv(output_folder + 'sea_ice_nh.csv') 
+df_shcut = pd.read_csv(output_folder + 'sea_ice_sh.csv') 
+sum_co2_df = pd.read_csv(output_folder + 'summed_co2.csv')
+
+# Merge the DataFrames on the 'Year' column for plot construction
+merged_nh = pd.merge(df_nhcut, sum_co2_df, on='Year')
+merged_sh = pd.merge(df_shcut, sum_co2_df, on='Year')
+
+
+# Plot the data on 2 figures, one for the Northern Hemisphere and one for the Sourthern Hemisphere
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
+
+## Plot Northern Hemisphere sea ice extent vs CO2
+# Sea ice extent over Time
+ax1.plot(merged_nh['Year'], merged_nh['NH_Extent'], color='blue', label='NH Sea Ice Extent')
+ax1.set_xlabel('Year')
+ax1.set_ylabel('NH Sea Ice Extent (million sq km)', color='blue')                                 ## Units??
+ax1.tick_params(axis='y', labelcolor='blue')
+
+# CO2 over Time
+ax1_twin = ax1.twinx()
+ax1_twin.plot(merged_nh['Year'], merged_nh['CO2'], color='red', label='CO2')
+ax1_twin.set_ylabel('CO2 (Gt)', color='red')
+ax1_twin.tick_params(axis='y', labelcolor='red')
+
+# Set a figure title and legends 
+ax1.set_title('Northern Hemisphere\'s Sea Ice Extent and CO2 Emissions Over Time')
+ax1.legend(loc='upper left')
+ax1_twin.legend(loc='upper right')
+
+# Second plot Sourthern Hemisphere sea ice extent vs CO2
+# Sea ice extent over Time
+ax2.plot(merged_sh['Year'], merged_sh['SH_Extent'], color='green', label='SH Sea Ice Extent')
+ax2.set_xlabel('Year')
+ax2.set_ylabel('SH Sea Ice Extent (million sq km)', color='green')
+ax2.tick_params(axis='y', labelcolor='green')
+ax2.xaxis.set_major_locator(MaxNLocator(nbins=10))
+
+# CO2 over Time
+ax2_twin = ax2.twinx()
+ax2_twin.plot(merged_sh['Year'], merged_sh['CO2'], color='red', label='CO2')
+ax2_twin.set_ylabel('CO2 (Gt)', color='red')
+ax2_twin.tick_params(axis='y', labelcolor='red')
+
+# Set a figure title and legends 
+ax2.set_title('Southern Hemisphere\'s Sea Ice Extent and CO2 Emissions Over Time')
+ax2.legend(loc='upper left')
+ax2_twin.legend(loc='upper right')
+
+# Show plot
+plt.tight_layout()
+plt.savefig(output_folder + 'Correlations.png')
+print('Ploted. Saved.')
+plt.show()
+
+
+
+# --------- Extracting Data from ASC file (.asc) ------------------------------------------------------------
+
+# Set input and output file names
+input = 'aravg.ann.ocean.90S.60S.v6.0.0.202410.asc'
+output = 'ocean_temp_annual_SPole.csv'
+
+# Make an empty entry list that can be filled
+list_entries = []
+
+# Reading the ASC file
+with open(data_folder + input, 'r') as file:
+    content = file.readlines()
+    for line in content:
+        #print(line.strip())
+        list_entries.append(line.strip())
+#print(list_entries)
+
+split_data = []
+
+for row in list_entries:
+    values = row.strip().split()
+    years = values[0]
+    for i,value in enumerate(values[1:]):
+        split_data.append([f'{years}',value])
+
+temp_df = pd.DataFrame(split_data, columns=['Year','Temperature'])
+#if temp_df['Value'] == '-999.000000':
+temp_df = temp_df[temp_df['Temperature'] != '-999.000000']
+temp_df.reset_index(drop=True, inplace=True)
+
+#print(temp_df[120:130])
+
+ocean_temp_cut = temp_df[129:174]
+ocean_temp_cut.reset_index(drop=True, inplace=True)
+print(ocean_temp_cut)
+
+ocean_temp_cut.to_csv(output_folder + output, index=False)
+print('Accomplished.')
+
+
+
+'''
+# Read the text file
+with open(data_folder + input, 'r') as file:
+    rows = file.readlines()
+
+#print(rows[:5])
+
+# Remove headers and things
+data_rows = rows[116:161]
+
+# print(data_rows[-5:])
+# print(data_rows[:5])
+
+# Select the wanted data
+months = ['01','02','03','04','05','06','07','08', '09','10','11','12']
+timeXdata = []
+
+for row in data_rows:
+    values = row.strip().split()
+    years = values[0]
+    for i,value in enumerate(values[1:]):
+        timeXdata.append([f'{years}-{months[i]}',value])
+
+#print(timeXdata[:5])
+
+# Make into a dataframe
+nino_df = pd.DataFrame(timeXdata, columns=['Date','Std_Value'])
+
+# Convert to CSV file
+nino_df.to_csv(output_folder+output, index=False)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
