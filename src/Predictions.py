558711f4c9bc0eb3ea96a96029146797f2a470b3
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-12-17 21:02:42 +0100

    creation of predictions.csv

--- src/Predictions.py
+++ src/Predictions.py
@@ -3,6 +3,7 @@
 #======================================================================================================================
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
@@ -28,6 +29,8 @@ b = coefficients['bN']
 
 # Target years
 target_years = range(2023, 2101, 1)
+
+#----------------------- Predictions plots ------------------------
 
 # optimistic scenario : SSP1
 years_spp1 = np.array([2023, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100])
@@ -56,10 +59,6 @@ ax1.set_title('CO₂ Emission Scenarios from 2023 to 2100')
 ax1.legend()
 ax1.grid(True)
 
-# Afficher les résultats dans la console
-#for year, emission_ssp1, emission_ssp2, emission_ssp3 in zip(target_years, emissions_interp_ssp1, emissions_interp_ssp3, emissions_interp_ssp5):
-#    print(f"Année: {year}, Emission CO₂ SSP1: {emission_ssp1:.2f} Gt, Emission CO₂ SSP2: {emission_ssp2:.2f} Gt, Emission CO₂ SSP3: {emission_ssp3:.2f} Gt")
-
 # Calculate sea ice extent for each scenario
 sea_ice_extent_ssp1 = m * emissions_interp_ssp1 + b
 sea_ice_extent_ssp3 = m * emissions_interp_ssp3 + b
@@ -78,5 +77,40 @@ ax2.grid(True)
 plt.savefig(output_folder + '7_NH_Predictions_plot.png')
 plt.show()
 print('Predictions ploted and saved.')
+
+#----------------------- Predictions file ------------------------
+data_nh = pd.read_csv(data_folder + 'NH_Data.csv', delimiter=',')
+
+year_existing = data_nh['Year'].values
+y_existing = data_nh['Sea_Ice_Extent'].values
+CO2_existing = data_nh['CO2'].values
+
+# Combine existing data and predictions
+all_years = np.concatenate((year_existing, target_years))
+all_y_ssp1 = np.concatenate((y_existing, sea_ice_extent_ssp1))
+all_y_ssp3 = np.concatenate((y_existing, sea_ice_extent_ssp3))
+all_y_ssp5 = np.concatenate((y_existing, sea_ice_extent_ssp5))
+all_CO2_ssp1 = np.concatenate((CO2_existing, emissions_interp_ssp1)) 
+all_CO2_ssp3 = np.concatenate((CO2_existing, emissions_interp_ssp3))
+all_CO2_ssp5 = np.concatenate((CO2_existing, emissions_interp_ssp5))
+
+# Create a DataFrame with all the data
+extended_data = pd.DataFrame({
+    'Year': all_years,
+    'y_SSP1': all_y_ssp1,
+    'y_SSP3': all_y_ssp3,
+    'y_SSP5': all_y_ssp5,
+    'CO2_SSP1': all_CO2_ssp1,
+    'CO2_SSP3': all_CO2_ssp3,
+    'CO2_SSP5': all_CO2_ssp5
+})
+
+# Save the DataFrame to a CSV file
+extended_data.to_csv(f'{output_folder}/Predictions.csv', index=False)
+
+print(f"Extended predictions saved to {output_folder}/Predictions.csv")
+
+
+
 
 
