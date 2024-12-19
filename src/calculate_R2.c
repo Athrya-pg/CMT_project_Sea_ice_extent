0af2d61d9bf316b4eee5264c60a45845a8eee248
Chloé Prats-Genre <chloe.prats-genre@epfl.ch>
2024-12-19 23:14:48 +0100

    Update calculate_R2.c

--- src/calculate_R2.c
+++ src/calculate_R2.c
@@ -14,15 +14,17 @@ double mean(double *values, int n) {
     return sum / n;
 }
 
-//Function to calculate R2, the coefficient of determination
-double calculate_R2(double* y_true, double* y_pred, int n){
-    double ssr = 0.0;
-    double sse = 0.0;
-    double y_mean = mean(y_true, n);
+// Function to calculate R2, the coefficient of determination
+double calculate_R2(double* y_true, double* y_pred, int n) {
+    // Initialize variables : ssr = Sum of squares of residuals, sse = Total sum of squares, y_mean = Mean of true values
+    double ssr = 0.0; 
+    double sse = 0.0; 
+    double y_mean = mean(y_true, n); 
 
+    // Calculate ssr and sse
     for (int i = 0; i < n; i++) {
-        ssr += pow(y_true[i] - y_pred[i], 2);
-        sse += pow(y_true[i] - y_mean, 2);
+        ssr += pow(y_true[i] - y_pred[i], 2); 
+        sse += pow(y_true[i] - y_mean, 2); 
     }
     return 1 - (ssr / sse);
 }
